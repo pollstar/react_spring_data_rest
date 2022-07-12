a72038d618df8a1dbacd2527d869dad91bb5f205
Pavel Starovoitov <pollstar27@gmail.com>
2022-07-12 22:49:33 +0300

    Initial commit

---
+++ src/main/java/ua/pollstar/react_spring_data_rest/ReactSpringDataRestApplication.java
@@ -0,0 +1,13 @@
+package ua.pollstar.react_spring_data_rest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactSpringDataRestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReactSpringDataRestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ua/pollstar/react_spring_data_rest/ReactSpringDataRestApplicationTests.java
@@ -0,0 +1,13 @@
+package ua.pollstar.react_spring_data_rest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactSpringDataRestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
